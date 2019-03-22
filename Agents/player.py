7dd4c6c20903aa08ed172216c5747ede73766c10
Andrew Berkow <andrewberkow@Andrews-MacBook-Pro.local>
2019-03-21 22:04:02 -0400

    Ensure random moves work.

--- Agents/player.py
+++ Agents/player.py
@@ -1,9 +1,24 @@
+import random
+'''
+PEAS Description for player agent in a game of tic tac toe.
+Performance measure:
+Environment: ticTacToeBoard object
+Actuators: Make move
+Sensors: Game is fully observable and the play can get access to the state at any time.
+'''
+
 class Player:
-    def __init__(self, environment, player_x = True):
-        self.mark = "X" if player_x else "0"
+    def __init__(self, environment, mark):
+        self.mark = mark
         self.environment = environment
 
     def move(self):
+        legal_move_made = False
+        while not legal_move_made:
+            xcord = random.randint(0,2)
+            ycord = random.randint(0, 2)
+            legal_move_made = self.environment.insert(self.mark, xcord, ycord)
+
         return True
 
 
--- play.py
+++ play.py
@@ -2,6 +2,16 @@ from Environments.ticTacToeBoard import TicTacToeBoard
 from Agents.player import Player
 
 board = TicTacToeBoard()
+ai_player = Player("X")
+
+# ai_player.move()
+# board.print_state()
+# print(board.check_winner("X"))
+#
+# move(board)
+# board.print_state()
+# print(board.check_winner("O"))
+
 board.insert("X", 0, 0)
 board.print_state()
 print(board.check_winner("X"))
@@ -26,4 +36,13 @@ print(board.check_winner("O"))
 board.insert("O", 2, 1)
 board.print_state()
 print(board.check_winner("O"))
+
+#
+# def move(board):
+#     legal_move_made = False
+#     while not legal_move_made:
+#         xcord = random.randint(0, 3)
+#         ycord = random.randint(0, 3)
+#         legal_move_made = self.environment.insert("0", xcord, ycord)
+#     return True
 
