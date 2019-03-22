deee8545aa56424eae0b973dac480ac9fa2f1b03
Andrew Berkow <andrewberkow@Andrews-MacBook-Pro.local>
2019-03-21 22:05:12 -0400

    test

--- play.py
+++ play.py
@@ -2,47 +2,39 @@ from Environments.ticTacToeBoard import TicTacToeBoard
 from Agents.player import Player
 
 board = TicTacToeBoard()
-ai_player = Player("X")
+ai_player = Player(board, "X")
+ai_player2 = Player(board, "O")
 
-# ai_player.move()
+for x in range(4):
+    ai_player.move()
+    board.print_state()
+    print(board.check_winner("X"))
+
+    ai_player2.move()
+    board.print_state()
+    print(board.check_winner("O"))
+
+# board.insert("X", 0, 0)
 # board.print_state()
 # print(board.check_winner("X"))
 #
-# move(board)
+# board.insert("O", 0, 1)
 # board.print_state()
 # print(board.check_winner("O"))
-
-board.insert("X", 0, 0)
-board.print_state()
-print(board.check_winner("X"))
-
-board.insert("O", 0, 1)
-board.print_state()
-print(board.check_winner("O"))
-
-
-board.insert("X", 1, 0)
-board.print_state()
-print(board.check_winner("X"))
-
-board.insert("X", 2, 0)
-board.print_state()
-print(board.check_winner("X"))
-
-board.insert("O", 1, 1)
-board.print_state()
-print(board.check_winner("O"))
-
-board.insert("O", 2, 1)
-board.print_state()
-print(board.check_winner("O"))
-
 #
-# def move(board):
-#     legal_move_made = False
-#     while not legal_move_made:
-#         xcord = random.randint(0, 3)
-#         ycord = random.randint(0, 3)
-#         legal_move_made = self.environment.insert("0", xcord, ycord)
-#     return True
-
+#
+# board.insert("X", 1, 0)
+# board.print_state()
+# print(board.check_winner("X"))
+#
+# board.insert("X", 2, 0)
+# board.print_state()
+# print(board.check_winner("X"))
+#
+# board.insert("O", 1, 1)
+# board.print_state()
+# print(board.check_winner("O"))
+#
+# board.insert("O", 2, 1)
+# board.print_state()
+# print(board.check_winner("O"))
